08bb3c83f9644fff50d38ed6862486bcf2f30c09
akbar <akbarsmp27@gmail.com>
2024-04-27 23:07:16 +0700

    initial commit.

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "flake for dotfiles";
+
+  inputs = {
+    # using the currently up-to-date packages on `nixpkgs`
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+    # home manager that follows nixpkgs version 
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+	let 
+		system = "x86_64-linux";
+		pkgs = nixpkgs.legacyPackages.${system};
+	in
+	 {
+	# configurations for home-manager 
+	homeConfigurations = {
+		akbar = home-manager.lib.homeManagerConfiguration {
+			inherit pkgs;
+			modules = [ ./home.nix ];
+		};
+	};
+  };
+}
